b8d379532a22fbd7b118447b2baa063c5fb41d75
Useol <usus_us@naver.com>
2025-01-10 17:36:03 +0900

    쌰갈

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fficial-naver/',
+  base: './',
   build: {
     outDir: 'dist', // 빌드 결과물을 docs 폴더로 지정
   },
