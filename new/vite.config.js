e4443f9b757a3b6aeaa05803acb45ca35ad85dc6
Useol <usus_us@naver.com>
2025-01-10 16:51:43 +0900

    first

--- new/vite.config.js
+++ new/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs', // 빌드 결과물을 docs 폴더로 지정
+  },
 })
